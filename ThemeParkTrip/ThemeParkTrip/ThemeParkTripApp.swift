0364d7df761e71e76e60c957c8788c0131f90455
Oren Leavitt <oleavitt@ix.netcom.com>
2024-09-18 23:05:53 -0500

    Initial project skeleton

---
+++ ThemeParkTrip/ThemeParkTrip/ThemeParkTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeParkTripApp.swift
+//  ThemeParkTrip
+//
+//  Created by Oren Leavitt on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ThemeParkTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThemeParkTrip/ThemeParkTripTests/ThemeParkTripTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeParkTripTests.swift
+//  ThemeParkTripTests
+//
+//  Created by Oren Leavitt on 9/18/24.
+//
+
+import Testing
+@testable import ThemeParkTrip
+
+struct ThemeParkTripTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
